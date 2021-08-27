c3b4b75a4154a04822aaee308998c0f2867a15fe
thiago <tguedes6@hotmail.com>
2021-08-27 19:19:04 -0300

    first commit

---
+++ projects/angular-lib/src/lib/angular-lib.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { AngularLibComponent } from './angular-lib.component';
+
+
+
+@NgModule({
+  declarations: [
+    AngularLibComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    AngularLibComponent
+  ]
+})
+export class AngularLibModule { }
---
+++ projects/angular-lib/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of angular-lib
+ */
+
+export * from './lib/angular-lib.service';
+export * from './lib/angular-lib.component';
+export * from './lib/angular-lib.module';
