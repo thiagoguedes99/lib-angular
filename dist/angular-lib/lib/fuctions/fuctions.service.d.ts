2b006a898a7b0f2bd242320980270862bb19fcb9
thiago <tguedes6@hotmail.com>
2021-09-11 23:15:18 -0300

    correction default is-mobile function

--- dist/angular-lib/lib/fuctions/fuctions.service.d.ts
+++ dist/angular-lib/lib/fuctions/fuctions.service.d.ts
@@ -5,7 +5,7 @@ export declare class FuctionsService {
     /** Recebe a data em formato americano e devolte a data em formato brasileiro com descrição do mês */
     fullDate(date?: string): string;
     /** Valida se o device tem um tela tamanho mobile, tamanho default 960px de largura */
-    isMobile(date: string): boolean;
+    isMobile(date?: string): boolean;
     static ɵfac: i0.ɵɵFactoryDeclaration<FuctionsService, never>;
     static ɵprov: i0.ɵɵInjectableDeclaration<FuctionsService>;
 }
